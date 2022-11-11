0303184f4d25b05b0759fdc814ec742975da8b17
Jay Boisjoli <jeremie.boisjoli@bell.ca>
2022-11-11 14:33:29 -0500

    Initial Commit

---
+++ jboisjoli-babbel/Document.swift
@@ -0,0 +1,21 @@
+//
+//  Document.swift
+//  jboisjoli-babbel
+//
+//  Created by Jay Boisjoli on 2022-11-11.
+//
+
+import UIKit
+
+class Document: UIDocument {
+    
+    override func contents(forType typeName: String) throws -> Any {
+        // Encode your document with an instance of NSData or NSFileWrapper
+        return Data()
+    }
+    
+    override func load(fromContents contents: Any, ofType typeName: String?) throws {
+        // Load your document from contents
+    }
+}
+
